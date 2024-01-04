37cdba516b1bd1b29437544468d915fcf8a7c3dc
Kuroikenshiga <juniorsilvapalafoz@gmail.com>
2024-01-04 11:24:12 -0300

    04-01-24

--- Day4/src/day4/Day4.java
+++ Day4/src/day4/Day4.java
@@ -1,20 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package day4;
-
-/**
- *
- * @author TeresaShySmile
- */
+import java.util.*;
 public class Day4 {
+public static void main(String[] args) {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    int[] a = {1,2},b = {3,4};
+    //1,2,7,8,9
+    System.out.println(medianOfTwoSortedArrays(a, b));
+
+}
+    public static double medianOfTwoSortedArrays(int[]a,int[]b){
+        int[] c = new int[a.length+b.length];
+        int[] aux;
+        int j = 0,k = 0;
+        
+        if(a.length > b.length){
+            aux = a;
+            a = b;
+            b = aux;
+        }
+        
+        
+        for(int i = 0;i < c.length;i++){
+            if(j < a.length){
+                if(k < b.length){
+                    if(a[j] > b[k]){
+                        c[i] = b[k];
+                        k++;
+                    }
+                    else{
+                        c[i] = a[j];
+                        j++;
+                    }
+                }
+                else{
+                    c[i] = a[j];
+                    j++;
+                }
+            }
+            else{
+                c[i] = b[k];
+                k++;
+            }
+       }
+        return c.length%2 == 0?(double)(c[(c.length/2) - 1] + c[(c.length/2)])/2:c[(c.length/2)];
+}
 }
